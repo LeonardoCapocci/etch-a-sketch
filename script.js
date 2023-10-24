f9dfc1c8677d0a9f1bf5237877b8464a26d0b4af
LeonardoCapocci <leocap2001@gmail.com>
2023-10-24 12:21:09 -0700

    added a size button

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 const container = document.querySelector("#container");
-
-n = 100
-
-for (let i = 0; i < n; i++) {
-  const divRow = document.createElement("div");
-  divRow.className = "row";
-  container.appendChild(divRow);
-  for (let j = 0; j < n; j++) {
-    const divCell = document.createElement("div");
-    divRow.appendChild(divCell);
-    divCell.className = "cell";
+etchASketch();
+function etchASketch(n=24) {
+  for (let i = 0; i < n; i++) {
+    const divRow = document.createElement("div");
+    divRow.className = "row";
+    container.appendChild(divRow);
+    for (let j = 0; j < n; j++) {
+      const divCell = document.createElement("div");
+      divRow.appendChild(divCell);
+      divCell.className = "cell";
+    }
   }
+  const divCells = document.querySelectorAll(".cell");
+  divCells.forEach((divCell) => {
+    divCell.addEventListener('mouseover', () => {
+        divCell.style.backgroundColor = "red";
+    })
+  });
 }
 
-const divCells = document.querySelectorAll(".cell");
-divCells.forEach((divCell) => {
-  divCell.addEventListener('mouseover', () => {
-      divCell.style.backgroundColor = "red";
-  })
-});
+const sizeButton = document.querySelector("#size-button");
+sizeButton.addEventListener('click', () => {
+  container.innerHTML = "";
+  const newSize = parseInt(prompt("choose a size for the grid"));
+  etchASketch(newSize);
+})
